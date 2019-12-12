5f76c33a9bf9fd503c36e7ea75f055277fd96c9e
yuanningma <yuanning_ma@brown.edu>
2019-12-12 03:10:56 -0500

    84 percent acc rnn

--- main.py
+++ main.py
@@ -26,18 +26,6 @@ def test(model, test_inputs, test_labels):
     return model.accuracy(logits, test_labels)
 
 def main():
-    if len(sys.argv) != 2 or sys.argv[1] not in {"LINEAR","CNN", "RNN"}:
-        print("USAGE: python main.py <Model Type>")
-        print("<Model Type>: [LINEAR/CNN/RNN]")
-        exit()
-
-    print("Running preprocessing...")
-    # train_inputs, train_labels, test_inputs, test_labels = get_data("data/genres.tar")
-    # train_inputs, train_labels, test_inputs, test_labels = get_data("genres.gz")
-    train_inputs, train_labels, test_inputs, test_labels = get_rnn_data("data/genres.tar")
-
-    print("Preprocessing complete.")
-
     if sys.argv[1] == "LINEAR":
         model = Linear()
         num_epochs = 100
@@ -46,11 +34,79 @@ def main():
         num_epochs = 1
     elif sys.argv[1] == "RNN":
         model = RNN()
-        num_epochs = 1
+    if len(sys.argv) != 2 or sys.argv[1] not in {"LINEAR","CNN", "RNN"}:
+        print("USAGE: python main.py <Model Type>")
+        print("<Model Type>: [LINEAR/CNN/RNN]")
+        exit()
+
+    print("Running preprocessing...")
+    # train_inputs, train_labels, test_inputs, test_labels = get_data("data/genres.tar")
+    # train_inputs, train_labels, test_inputs, test_labels = get_data("genres.gz")
+    # train_inputs, train_labels, test_inputs, test_labels = get_rnn_data("data/genres.tar")
+    if sys.argv[1] == "RNN":
+        train_inputs, train_labels, test_inputs, test_labels = get_rnn_data("data/genres.tar.gz")
+    else:
+        train_inputs, train_labels, test_inputs, test_labels = get_data("data/genres.tar")
+
+    print("Preprocessing complete.")
 
 
-    for _ in range(num_epochs):
-        train(model, train_inputs, train_labels)
+
+
+
+
+
+    if sys.argv[1] == "RNN":
+        # train_inputs = train_inputs.reshape(train_inputs.shape[0], train_inputs.shape[1], 1)
+        # test_inputs = test_inputs.reshape(test_inputs.shape[0], test_inputs.shape[1], 1)
+        num_epochs = 10
+        model = tf.keras.Sequential()
+        model.add(tf.keras.layers.Dense(units=256))
+        model.add(tf.keras.layers.BatchNormalization())
+        model.add(tf.keras.layers.LeakyReLU(0.2))
+        model.add(tf.keras.layers.Reshape((16, 16)))
+        # model.add(tf.keras.layers.GRU(90, use_bias=True, dropout=0.5, recurrent_dropout=0.2))
+        # model.add(tf.keras.layers.Dense(60, kernel_regularizer=tf.keras.regularizers.l2(.01), use_bias=True))
+        # model.add(tf.keras.layers.LeakyReLU(0.5))
+        # model.add(tf.keras.layers.Dense(10, activation='softmax', use_bias=True))
+
+
+        model.add(tf.keras.layers.LSTM(units=128, dropout=0.05, recurrent_dropout=0.35, return_sequences=True))
+        model.add(tf.keras.layers.LSTM(units=32,  dropout=0.05, recurrent_dropout=0.35, return_sequences=False))
+        model.add(tf.keras.layers.Dense(units=10, activation="softmax"))
+        #
+        opt = tf.keras.optimizers.Adam(lr=.01)
+        model.compile(optimizer=opt, loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+        history=model.fit(train_inputs, train_labels, epochs=num_epochs, batch_size=100)
+        model.summary()
+        test_loss, test_acc = model.evaluate(test_inputs, test_labels)
+        print('test acc: ', test_acc)
+
+        print(history.history.keys())
+        plt.plot(history.history['accuracy'])
+        plt.title('Model accuracy')
+        plt.ylabel('Accuracy')
+        plt.xlabel('Epoch')
+        plt.legend(['Train', 'Test'], loc='upper left')
+        plt.show()
+
+        # Plot training & validation loss values
+        plt.plot(history.history['loss'])
+        plt.title('Model loss')
+        plt.ylabel('Loss')
+        plt.xlabel('Epoch')
+        plt.legend(['Train', 'Test'], loc='upper left')
+        plt.show()
+        #
+        # model.fit(train_inputs, train_labels, batch_size = 35, verbose=1)
+        #
+        # score, acc = model.evaluate(test_inputs, test_labels, batch_size=35, verbose=1)
+        # print("Test acc: ", acc)
+    else:
+        for _ in range(num_epochs):
+            train(model, train_inputs, train_labels)
+        print(test(model, test_inputs, test_labels))
 
     # history = model.fit(train_inputs, train_labels, validation_split = 0.2, epochs=num_epochs, batch_size=model.batch_size, verbose=1)
     # plt.plot(history.history['acc'])
@@ -72,7 +128,7 @@ def main():
     #
     # from keras.utils import plot_model
     # plot_model(model, to_file="model.png")
-    print(test(model, test_inputs, test_labels))
+
 
 
 if __name__ == '__main__':
--- preprocess.py
+++ preprocess.py
@@ -2,61 +2,118 @@ import librosa
 import tarfile
 import soundfile as sf
 import io
+import os
 import numpy as np
 import tensorflow as tf
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+import pandas as pd
+import csv
 
 genre_ids = {"rock": 0, "reggae": 1, "pop": 2, "metal": 3, "jazz": 4, "hiphop": 5, "disco" : 6, "country": 7, "classical": 8, "blues": 9}
 total_frames = 661794
 total_mfcc_frames = 1293
 # Given 30 second audio clip, how many datapoints to break up into
-divisor = 10
+divisor = 12
 frame_portion = 1.0 / divisor
 parsed_frames = int(total_frames * frame_portion)
 parsed_mfcc_frames = int(total_mfcc_frames * frame_portion)
 test_split = 0.8
 
 def get_rnn_data(file_name):
-    inputs = []
-    labels = []
-    tar = tarfile.open(file_name)
-    for member in tar.getmembers():
-        if ".wav" in member.name:
-            f=tar.extractfile(member)
-            if f != None:
-                buff = io.BytesIO(f.read())
-
-                # Should experiment with sf.read
-                # Default number of frames is 661794 (~30 seconds)
-                # For ~5 seconds, number of frames is 110299
-                time_series, sample_rate = sf.read(buff)
-                # Converter: mfcc or chromagram
-                # Each converted_mfcc is 20 by num_frames
-                # num_frames is 216 for ~5 seconds and 1293 for ~30 seconds
-                converted_mfcc  = librosa.feature.mfcc(time_series)
-                label_id = genre_ids[member.name.split('/')[1]]
-
+    header = 'filename chroma_stft rmse spectral_centroid spectral_bandwidth rolloff zero_crossing_rate'
+    for i in range(1, 21):
+        header += f' mfcc{i}'
+    header += ' label'
+    header = header.split()
+    if not (os.path.isfile('data2.csv')):
+        file = open('data2.csv', 'w', newline='')
+        with file:
+            writer = csv.writer(file)
+            writer.writerow(header)
+        genres = 'blues classical country disco hiphop jazz metal pop reggae rock'.split()
+        for g in genres:
+            for filename in os.listdir(f'./data/genres/{g}'):
+                songname = f'./data/genres/{g}/{filename}'
+                y, sr = librosa.load(songname, mono=True, duration=30)
+                chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
+                #print("chroma: ", np.shape(chroma_stft))
+                rms = librosa.feature.rms(y=y)
+                #print("rms: ", np.shape(rms))
+                spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
+                #print("spec_cent: ", np.shape(spec_cent))
+                spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
+                #print("spec_bw: ", np.shape(spec_bw))
+                rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+                #print("rolloff: ", np.shape(rolloff))
+                zcr = librosa.feature.zero_crossing_rate(y)
+                #print("zcr: ", np.shape(zcr))
+                mfcc = librosa.feature.mfcc(y=y, sr=sr)
+                lenny = int(len(chroma_stft)/divisor)
                 for i in range(divisor):
-                    # no averaging
-                    pt = converted_mfcc[:, (i * parsed_mfcc_frames): ((i + 1) * parsed_mfcc_frames)]
-                    # print(pt.shape)
-                    inputs.append(pt)
-                    labels.append(label_id)
-                    # spliced_datapoint = np.average(converted_mfcc[:, (i * parsed_mfcc_frames): ((i + 1) * parsed_mfcc_frames)], axis=1)#.reshape((20, 1))
-                    # inputs.append(spliced_datapoint)
-                    # labels.append(label_id)
-                
-    tar.close()
+                    c = chroma_stft[i*lenny:(i+1)*lenny]
+                    r =  rms[i*lenny:(i+1)*lenny]
+                    # print("chroma: ", chroma_stft[i*lenny:(i+1)*lenny], len(c))
+                    # print("rms: ", rms[i*lenny:(i+1)*lenny], len(r))
+                    to_append = f'{filename} {np.mean(chroma_stft[i*lenny:(i+1)*lenny])} {np.mean(rms)} {np.mean(spec_cent)} {np.mean(spec_bw)} {np.mean(rolloff)} {np.mean(zcr)}'
+                    for e in mfcc:
+                        to_append += f' {np.mean(e[i*lenny:(i+1)*lenny])}'
+                    to_append += f' {g}'
+                    file = open('data2.csv', 'a', newline='')
+                    with file:
+                        writer = csv.writer(file)
+                        writer.writerow(to_append.split())
+                # songname = f'./data/genres/{g}/{filename}'
+                # y, sr = librosa.load(songname, mono=True, duration=30)
+                # chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
+                # rms = librosa.feature.rms(y=y)
+                # spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
+                # spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
+                # rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+                # zcr = librosa.feature.zero_crossing_rate(y)
+                # mfcc = librosa.feature.mfcc(y=y, sr=sr)
+                # to_append = f'{filename} {np.mean(chroma_stft)} {np.mean(rms)} {np.mean(spec_cent)} {np.mean(spec_bw)} {np.mean(rolloff)} {np.mean(zcr)}'
+                # for e in mfcc:
+                #     to_append += f' {np.mean(e)}'
+                # to_append += f' {g}'
+                # file = open('data.csv', 'a', newline='')
+                # with file:
+                #     writer = csv.writer(file)
+                #     writer.writerow(to_append.split())
 
-    num_points = len(labels)
-    split_index = int(test_split * num_points)
-    indices = range(0, num_points)
 
-    indices = tf.random.shuffle(indices)
+    # reading dataset from csv
 
-    shuffled_inputs = tf.gather(inputs, indices)
-    shuffled_labels = tf.gather(labels, indices)
+    data = pd.read_csv('data2.csv')
+    data.head()
 
-    return shuffled_inputs[:split_index], shuffled_labels[:split_index], shuffled_inputs[split_index:], shuffled_labels[split_index:]
+    # Dropping unneccesary columns
+    data = data.drop(['filename'],axis=1)
+    data = data.drop(['rmse'], axis=1)
+    data = data.drop(['spectral_bandwidth'], axis=1)
+    data = data.drop(['rolloff'], axis=1)
+    data = data.drop(['zero_crossing_rate'], axis=1)
+    data = data.drop(['spectral_centroid'], axis=1)
+    data.head()
+
+    genre_list = data.iloc[:, -1]
+    encoder = LabelEncoder()
+    y = encoder.fit_transform(genre_list)
+    print(y)
+
+    # normalizing
+    scaler = StandardScaler()
+    X = scaler.fit_transform(np.array(data.iloc[:, :-1], dtype = float))
+
+    # spliting of dataset into train and test dataset
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    print("x train size: ", X_train.shape)
+    print("y train size: ", y_train.shape)
+    return X_train, y_train, X_test, y_test
+
+
 
 
 def get_data(file_name):
